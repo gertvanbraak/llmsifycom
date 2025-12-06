c6373df244d8708114a14acf0b65e658070014d8
Gert van Braak <g.vanbraak@viaict.nl>
2025-12-06 13:03:08 +0100

    About pages edited

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "LLMSify helps you get listed in LLM.";
+export const SITE_DESCRIPTION = "We help you get your Large Language Model listed in popular LLM directories.";
